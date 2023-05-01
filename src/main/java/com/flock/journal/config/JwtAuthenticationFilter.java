6db3be4385c9f4d3ad595292b575fc4c9c65cc40
Maxim Elushev <maxim.elushev@yandex.ru>
2023-05-01 21:32:22 +0300

    create the JWT authentication filter

---
+++ src/main/java/com/flock/journal/config/JwtAuthenticationFilter.java
@@ -0,0 +1,27 @@
+package com.flock.journal.config;
+
+import java.io.IOException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+  @Override
+  protected void doFilterInternal(
+      @NonNull HttpServletRequest request,
+      @NonNull HttpServletResponse response,
+      @NonNull FilterChain filterChain
+  ) throws ServletException, IOException {
+  }
+}
