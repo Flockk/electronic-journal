e3b345ccbafcff51312215cf106a80c29b0aacb1
Maxim Elushev <maxim.elushev@yandex.ru>
2023-05-02 00:20:01 +0300

    create the authentication controller

---
+++ src/main/java/com/flock/journal/controller/AuthenticationController.java
@@ -0,0 +1,31 @@
+package com.flock.journal.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.flock.journal.model.AuthenticationResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+  @PostMapping("/register")
+  public ResponseEntity<AuthenticationResponse> register(
+      @RequestBody RegisterRequest request
+  ) {
+    // TODO: Реализовать логику регистрации
+  }
+
+  @PostMapping("/authenticate")
+  public ResponseEntity<AuthenticationResponse> authenticate(
+      @RequestBody AuthenticationRequest request
+  ) {
+    // TODO: Реализовать логику аутентификации
+  }
+}
