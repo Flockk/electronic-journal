c1066b6547d23a7484e76e33487a9c09fd6507bf
Maxim Elushev <maxim.elushev@yandex.ru>
2023-05-02 00:54:42 +0300

    implement the register and authenticate methods

--- src/main/java/com/flock/journal/service/AuthenticationService.java
+++ src/main/java/com/flock/journal/service/AuthenticationService.java
@@ -1,10 +1,16 @@
 package com.flock.journal.service;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.flock.journal.model.Role;
+import com.flock.journal.model.User;
 import com.flock.journal.model.auth.AuthenticationRequest;
 import com.flock.journal.model.auth.AuthenticationResponse;
 import com.flock.journal.model.auth.RegisterRequest;
+import com.flock.journal.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,11 +18,38 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthenticationService {
 
+  private final UserRepository repository;
+  private final PasswordEncoder passwordEncoder;
+  private final JwtService jwtService;
+  private final AuthenticationManager authenticationManager;
+
   public AuthenticationResponse register(RegisterRequest request) {
-    return null;
+    var user = User.builder()
+        .firstname(request.getFirstname())
+        .lastname(request.getFirstname())
+        .login(request.getLogin())
+        .password(passwordEncoder.encode(request.getPassword()))
+        .role(Role.STUDENT)
+        .build();
+    repository.save(user);
+    var jwtToken = jwtService.generateToken(user);
+    return AuthenticationResponse.builder()
+        .token(jwtToken)
+        .build();
   }
 
   public AuthenticationResponse authenticate(AuthenticationRequest request) {
-    return null;
+    authenticationManager.authenticate(
+        new UsernamePasswordAuthenticationToken(
+            request.getLogin(),
+            request.getPassword()
+        )
+    );
+    var user = repository.findByLogin(request.getLogin())
+        .orElseThrow();
+    var jwtToken = jwtService.generateToken(user);
+    return AuthenticationResponse.builder()
+        .token(jwtToken)
+        .build();
   }
 }
