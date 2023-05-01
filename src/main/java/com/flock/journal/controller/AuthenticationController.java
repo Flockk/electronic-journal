83baa25151d04963a51eaecac97effcd24908ab0
Maxim Elushev <maxim.elushev@yandex.ru>
2023-05-02 00:29:49 +0300

    create the authentication models

--- src/main/java/com/flock/journal/controller/AuthenticationController.java
+++ src/main/java/com/flock/journal/controller/AuthenticationController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.flock.journal.model.AuthenticationResponse;
+import com.flock.journal.model.auth.AuthenticationResponse;
+import com.flock.journal.model.auth.AuthenticationRequest;
+import com.flock.journal.model.auth.RegisterRequest;
 
 import lombok.RequiredArgsConstructor;
 
