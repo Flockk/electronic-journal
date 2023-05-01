0edd0df2e98f76d5c1af1789b8fa8ab86a74e263
Maxim Elushev <maxim.elushev@yandex.ru>
2023-05-01 20:36:29 +0300

    extend the User to UserDetails

---
+++ src/main/java/com/flock/journal/model/Role.java
@@ -0,0 +1,8 @@
+package com.flock.journal.model;
+
+public enum Role {
+
+  ADMIN,
+  TEACHER,
+  STUDENT
+}
--- src/main/java/com/flock/journal/model/User.java
+++ src/main/java/com/flock/journal/model/User.java
@@ -1,16 +1,25 @@
 package com.flock.journal.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
+import java.util.Collection;
+import java.util.List;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.persistence.Id;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.GrantedAuthority;
 
 @Data
 @Builder
@@ -18,7 +27,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,4 +48,43 @@ public class User {
 
   @Column(name = "user_password")
   private String password;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "user_role")
+  private Role role;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(new SimpleGrantedAuthority(role.name()));
+  }
+
+  @Override
+  public String getUsername() {
+    return login;
+  }
+
+  @Override
+  public String getPassword() {
+    return password;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return false;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
