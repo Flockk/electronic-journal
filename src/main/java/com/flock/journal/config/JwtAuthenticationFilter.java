503c627de5321fac8f36297c89534c1614b26f01
Maxim Elushev <maxim.elushev@yandex.ru>
2023-05-01 22:40:31 +0300

    implement the UserDetailsService

--- src/main/java/com/flock/journal/config/JwtAuthenticationFilter.java
+++ src/main/java/com/flock/journal/config/JwtAuthenticationFilter.java
@@ -1,6 +1,5 @@
 package com.flock.journal.config;
 
-import com.flock.journal.service.JwtService;
 import java.io.IOException;
 
 import jakarta.servlet.FilterChain;
@@ -9,8 +8,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.flock.journal.service.JwtService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
   private final JwtService jwtService;
+  private final UserDetailsService userDetailsService;
 
   @Override
   protected void doFilterInternal(
@@ -35,5 +40,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
     jwt = authHeader.substring(7);
     userLogin = jwtService.extractUsername(jwt);
+    if (userLogin != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+      UserDetails userDetails = this.userDetailsService.loadUserByUsername(userLogin);
+    }
   }
 }
--- src/main/java/com/flock/journal/service/JwtService.java
+++ src/main/java/com/flock/journal/service/JwtService.java
@@ -5,11 +5,13 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -43,6 +45,19 @@ public class JwtService {
         .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
         .signWith(getSignInKey(), SignatureAlgorithm.ES256)
         .compact();
+  }
+
+  public boolean isTokenValid(String token, UserDetails userDetails) {
+    final String username = extractUsername(token);
+    return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+  }
+
+  private boolean isTokenExpired(String token) {
+    return extractExpiration(token).before(new Date());
+  }
+
+  private Date extractExpiration(String token) {
+    return extractClaim(token, Claims::getExpiration);
   }
 
   private Claims extractAllClaims(String token) {
