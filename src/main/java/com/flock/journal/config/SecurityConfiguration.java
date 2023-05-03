6bc1274183251b042a56645e4450b7577886562c
Maxim Elushev <maxim.elushev@yandex.ru>
2023-05-04 00:22:39 +0300

    changed permissions to @PreAuthorize

--- src/main/java/com/flock/journal/config/SecurityConfiguration.java
+++ src/main/java/com/flock/journal/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -28,25 +29,19 @@ import static com.flock.journal.model.Permission.STUDENT_CREATE;
 import static com.flock.journal.model.Permission.STUDENT_DELETE;
 import static com.flock.journal.model.Permission.STUDENT_READ;
 import static com.flock.journal.model.Permission.STUDENT_UPDATE;
-import static com.flock.journal.model.Permission.TEACHER_CREATE;
-import static com.flock.journal.model.Permission.TEACHER_DELETE;
-import static com.flock.journal.model.Permission.TEACHER_READ;
-import static com.flock.journal.model.Permission.TEACHER_UPDATE;
 
 import static com.flock.journal.model.Role.ADMIN;
 import static com.flock.journal.model.Role.STUDENT;
-import static com.flock.journal.model.Role.TEACHER;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
   private final JwtAuthenticationFilter jwtAuthFilter;
   private final AuthenticationProvider authenticationProvider;
   private final LogoutHandler logoutHandler;
-  private static final String ADMIN_API_PATTERN = "/api/v1/admin/**";
-  private static final String TEACHER_API_PATTERN = "/api/v1/teacher/**";
   private static final String STUDENT_API_PATTERN = "/api/v1/student/**";
 
   @Bean
@@ -57,24 +52,6 @@ public class SecurityConfiguration {
         .authorizeHttpRequests()
         .requestMatchers("/api/v1/auth/**")
         .permitAll()
-
-        .requestMatchers(ADMIN_API_PATTERN).hasRole(ADMIN.name())
-
-        .requestMatchers(GET, ADMIN_API_PATTERN).hasAuthority(ADMIN_READ.name())
-        .requestMatchers(POST, ADMIN_API_PATTERN).hasAuthority(ADMIN_CREATE.name())
-        .requestMatchers(PUT, ADMIN_API_PATTERN).hasAuthority(ADMIN_UPDATE.name())
-        .requestMatchers(DELETE, ADMIN_API_PATTERN).hasAuthority(ADMIN_DELETE.name())
-
-        .requestMatchers(TEACHER_API_PATTERN).hasAnyRole(ADMIN.name(), TEACHER.name())
-
-        .requestMatchers(GET, TEACHER_API_PATTERN)
-        .hasAnyAuthority(ADMIN_READ.name(), TEACHER_READ.name())
-        .requestMatchers(POST, TEACHER_API_PATTERN)
-        .hasAnyAuthority(ADMIN_CREATE.name(), TEACHER_CREATE.name())
-        .requestMatchers(PUT, TEACHER_API_PATTERN)
-        .hasAnyAuthority(ADMIN_UPDATE.name(), TEACHER_UPDATE.name())
-        .requestMatchers(DELETE, TEACHER_API_PATTERN)
-        .hasAnyAuthority(ADMIN_DELETE.name(), TEACHER_DELETE.name())
 
         .requestMatchers(STUDENT_API_PATTERN).hasAnyRole(ADMIN.name(), STUDENT.name())
 
--- src/main/java/com/flock/journal/controller/TeacherController.java
+++ src/main/java/com/flock/journal/controller/TeacherController.java
@@ -1,5 +1,6 @@
 package com.flock.journal.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,24 +10,29 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/teacher")
+@PreAuthorize("hasAnyRole('ADMIN', 'TEACHER')")
 public class TeacherController {
 
   @GetMapping
+  @PreAuthorize("hasAnyAuthority('admin:read', 'teacher:read')")
   public String get() {
     return "GET:: teacher controller";
   }
 
   @PostMapping
+  @PreAuthorize("hasAnyAuthority('admin:create', 'teacher:create')")
   public String post() {
     return "POST:: teacher controller";
   }
 
   @PutMapping
+  @PreAuthorize("hasAnyAuthority('admin:update', 'teacher:update')")
   public String put() {
     return "PUT:: teacher controller";
   }
 
   @DeleteMapping
+  @PreAuthorize("hasAnyAuthority('admin:delete', 'teacher:delete')")
   public String delete() {
     return "DELETE:: teacher controller";
   }
